1d47da87b7931f14b00fe0c1de87c4cf43948d33
Adam <adamaly93@gmail.com>
2025-11-15 15:54:15 +0000

    build: (painfully) got CMake working

---
+++ riscv.cmake
@@ -0,0 +1,18 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME Generic)
+
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER /opt/riscv32/bin/riscv32-unknown-elf-gcc)
+set(CMAKE_ASM_COMPILER /opt/riscv32/bin/riscv32-unknown-elf-as)
+set(CMAKE_CXX_COMPILER /opt/riscv32/bin/riscv32-unknown-elf-g++)
+
+# where is the target environment located
+set(CMAKE_FIND_ROOT_PATH /opt/riscv32/)
+
+# adjust the default behavior of the FIND_XXX() commands:
+# search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# search headers and libraries in the target environment
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
