59b1efd9e6793ef79c37126d1b0a1d2710391120
Adam <adamaly93@gmail.com>
2025-11-15 16:57:04 +0000

    build: cleanup

--- riscv.cmake
+++ riscv.cmake
@@ -1,13 +1,10 @@
-# the name of the target operating system
 set(CMAKE_SYSTEM_NAME Generic)
 
-# which compilers to use for C and C++
 set(CMAKE_C_COMPILER /opt/riscv32/bin/riscv32-unknown-elf-gcc)
 set(CMAKE_ASM_COMPILER /opt/riscv32/bin/riscv32-unknown-elf-as)
 set(CMAKE_CXX_COMPILER /opt/riscv32/bin/riscv32-unknown-elf-g++)
 set(CMAKE_LINKER /opt/riscv32/bin/riscv32-unknown-elf-ld)
 
-# where is the target environment located
 set(CMAKE_FIND_ROOT_PATH /opt/riscv32/)
 
 # adjust the default behavior of the FIND_XXX() commands:
