74bc0ea36d8988b1fa9f3656cebac9bb0fb21228
Adam <adamaly93@gmail.com>
2026-02-12 01:24:59 +0000

    pages: small iteration

--- src/kernel/page.c
+++ src/kernel/page.c
@@ -1,7 +1,23 @@
 #include <page.h>
 
+/* Physical Memory */
 char page_list[NO_FRAMES] __attribute__((section(".data.sys")));
 uint64_t *page_frames[NO_FRAMES] __attribute__((section(".data.sys")));
+
+/* Virtual Memory */
+uint64_t root_level_table[PAGE_SIZE_BYTES / 8] __attribute__((aligned(PAGE_SIZE_BYTES)));
+
+const uint64_t RWX_BITMASK = 0xFFFFFFFFFFFFFFF1;
+// const uint64_t VALID_BITMASK = 0xFFFFFFFFFFFFFFFE;
+
+__attribute__((section(".text.sys"))) void setup_page_table() {
+  for (unsigned int i = 0; i < sizeof(root_level_table) / sizeof(uint64_t); ++i) {
+    root_level_table[i] &= RWX_BITMASK;
+  }
+
+  // TODO: Finish this off
+  // root_level_table[0]
+}
 
 /* To be called with a 4K aligned address. */
 __attribute__((section(".text.sys"))) int populate_page_list(uint64_t startAddr) {
@@ -21,7 +37,7 @@ __attribute__((section(".text.sys"))) int populate_page_list(uint64_t startAddr)
 
 /* Finds a free page frame and returns a pointer to the beginning of it. */
 __attribute__((section(".text.sys"))) void *alloc_page() {
-  for (unsigned int i = 0; i < sizeof(page_frames); ++i) {
+  for (unsigned int i = 0; i < sizeof(page_frames) / sizeof(uint64_t *); ++i) {
     if (page_list[i] == 0) {
       return page_frames[i];
     }
