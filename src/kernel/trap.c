fc620a622b06c44df89a5de5411a6d680082ddf6
Adam <adamaly93@gmail.com>
2026-02-13 15:41:30 +0000

    trap: save U-MODE sp in pt_regs on trap entry

--- src/kernel/trap.c
+++ src/kernel/trap.c
@@ -2,13 +2,48 @@
 #include "syscalls.h"
 #include <stdint.h>
 
-__attribute__((section(".text.trap"))) void handle_syscall(uint64_t *regs) {
-  uint64_t syscall = regs[17];
+typedef struct pt_regs {
+    uint64_t x0;        /* x0  */
+    uint64_t ra;        /* x1  */
+    uint64_t sp;        /* x2  */
+    uint64_t gp;        /* x3  */
+    uint64_t tp;        /* x4  */
+    uint64_t t0;        /* x5  */
+    uint64_t t1;        /* x6  */
+    uint64_t t2;        /* x7  */
+    uint64_t s0;        /* x8  */
+    uint64_t s1;        /* x9  */
+    uint64_t a0;        /* x10 */
+    uint64_t a1;        /* x11 */
+    uint64_t a2;        /* x12 */
+    uint64_t a3;        /* x13 */
+    uint64_t a4;        /* x14 */
+    uint64_t a5;        /* x15 */
+    uint64_t a6;        /* x16 */
+    uint64_t a7;        /* x17 */
+    uint64_t s2;        /* x18 */
+    uint64_t s3;        /* x19 */
+    uint64_t s4;        /* x20 */
+    uint64_t s5;        /* x21 */
+    uint64_t s6;        /* x22 */
+    uint64_t s7;        /* x23 */
+    uint64_t s8;        /* x24 */
+    uint64_t s9;        /* x25 */
+    uint64_t s10;       /* x26 */
+    uint64_t s11;       /* x27 */
+    uint64_t t3;        /* x28 */
+    uint64_t t4;        /* x29 */
+    uint64_t t5;        /* x30 */
+    uint64_t t6;        /* x31 */
+} pt_regs;
+
+__attribute__((section(".text.trap"))) void handle_syscall(pt_regs *regs) {
+  uint64_t syscall = regs->a7;
 
   switch (syscall) {
   case SYS_PUTCHAR:
-    sbi_putchar((char)regs[10]);
-    regs[10] = 0;
+    sbi_putchar((char)regs->a0);
+    regs->a0 = 0; /* Return value in a0 */
     break;
   }
 }
